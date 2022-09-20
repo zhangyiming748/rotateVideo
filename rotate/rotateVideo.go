0d74fd17624e984ce0e01a9c3e0675328160a557
zen <zhangyiming748@gmail.com>
2022-09-20 16:43:35 +0800

    显示程序进程

--- rotate/rotateVideo.go
+++ rotate/rotateVideo.go
@@ -12,8 +12,9 @@ import (
 func Rotate(src, pattern, direction, dst, threads string) {
 	files := getFiles(src, pattern)
 	for index, file := range files {
-		log.Info.Printf("正在处理第 %d/%d 个文件\n", index+1, len(files))
+		log.Debug.Printf("正在处理第 %d/%d 个文件:%s\n", index+1, len(files), files)
 		rotate_help(src, dst, file, direction, threads)
+		log.Debug.Printf("处理完成第 %d/%d 个文件:%s\n", index+1, len(files), files)
 	}
 }
 func rotate_help(src, dst, file, direction, threads string) {
@@ -49,6 +50,7 @@ func rotate_help(src, dst, file, direction, threads string) {
 		//写成输出日志
 		t := string(tmp)
 		t = strings.Replace(t, "\u0000", "", -1)
+		log.Info.Printf("正在处理的文件:%s\n", file)
 		log.Info.Println(t)
 		if err != nil {
 			break
