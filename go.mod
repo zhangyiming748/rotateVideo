7f4df915451bd2d30ab06a2b2a41c49c64fd7bf9
zen <zhangyiming748@gmail.com>
2023-03-25 23:53:25 +0800

    脱离log

--- go.mod
+++ go.mod
@@ -4,14 +4,15 @@ go 1.19
 
 require (
 	github.com/zhangyiming748/GetFileInfo v0.0.13
-	github.com/zhangyiming748/log v0.0.5
 	github.com/zhangyiming748/replace v0.0.5
 	github.com/zhangyiming748/voiceAlert v0.0.12
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
 )
 
 require (
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	github.com/zhangyiming748/GetAllFolder v0.0.5 // indirect
+	github.com/zhangyiming748/log v0.0.5 // indirect
 	github.com/zhangyiming748/pretty v0.0.3 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
--- rotateVideo.go
+++ rotateVideo.go
@@ -1,21 +1,66 @@
 package rotateVideo
 
 import (
+	"fmt"
 	"github.com/zhangyiming748/GetFileInfo"
-	"github.com/zhangyiming748/log"
 	"github.com/zhangyiming748/replace"
 	"github.com/zhangyiming748/voiceAlert"
+	"golang.org/x/exp/slog"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
 )
 
+func init() {
+	logLevel := os.Getenv("LEVEL")
+	//var level slog.Level
+	var opt slog.HandlerOptions
+	switch logLevel {
+	case "Debug":
+		opt = slog.HandlerOptions{ // 自定义option
+			AddSource: true,
+			Level:     slog.LevelDebug, // slog 默认日志级别是 info
+		}
+	case "Info":
+		opt = slog.HandlerOptions{ // 自定义option
+			AddSource: true,
+			Level:     slog.LevelInfo, // slog 默认日志级别是 info
+		}
+	case "Warn":
+		opt = slog.HandlerOptions{ // 自定义option
+			AddSource: true,
+			Level:     slog.LevelWarn, // slog 默认日志级别是 info
+		}
+	case "Err":
+		opt = slog.HandlerOptions{ // 自定义option
+			AddSource: true,
+			Level:     slog.LevelError, // slog 默认日志级别是 info
+		}
+	default:
+		slog.Warn("需要正确设置环境变量 Debug,Info,Warn or Err")
+		slog.Info("默认使用Debug等级")
+		opt = slog.HandlerOptions{ // 自定义option
+			AddSource: true,
+			Level:     slog.LevelDebug, // slog 默认日志级别是 info
+		}
+
+	}
+	file := "rotateVideo.log"
+	logf, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
+	if err != nil {
+		panic(err)
+	}
+	//defer logf.Close() //如果不关闭可能造成内存泄露
+	logger := slog.New(opt.NewJSONHandler(io.MultiWriter(logf, os.Stdout)))
+	slog.SetDefault(logger)
+}
 func Rotate(src, pattern, direction, threads string) {
 	files := GetFileInfo.GetAllFileInfo(src, pattern)
 	for index, file := range files {
-		log.Debug.Printf("正在处理第 %d/%d 个文件\n", index+1, len(files))
+		slog.Info("正在处理第 %d/%d 个文件\n", index+1, len(files))
 		rotate(file, direction, threads)
-		log.Debug.Printf("处理完成第 %d/%d 个文件\n", index+1, len(files))
+		slog.Info("处理完成第 %d/%d 个文件\n", index+1, len(files))
 		voiceAlert.Customize("done", voiceAlert.Samantha)
 	}
 	voiceAlert.Customize("complete", voiceAlert.Samantha)
@@ -44,14 +89,16 @@ func rotate(in GetFileInfo.Info, direction, threads string) {
 	//	cmd = exec.Command("ffmpeg", "-threads", threads, "-i", in.FullPath, "-strict", "2", "-vf", "scale=-1:1080", "-vf", transport, "-c:v", "libx265", "-threads", threads, export)
 	//}
 	cmd = exec.Command("ffmpeg", "-threads", threads, "-i", in.FullPath, "-vf", transport, "-threads", threads, export)
-	log.Debug.Printf("开始处理文件%s\n生成的命令是:%s\n", in.FullPath, cmd)
+	slog.Info("开始处理文件", slog.Any("生成的命令", fmt.Sprint(cmd)))
 	stdout, err := cmd.StdoutPipe()
 	cmd.Stderr = cmd.Stdout
 	if err != nil {
-		log.Warn.Panicf("cmd.StdoutPipe产生的错误:%v", err)
+		slog.Warn("cmd.StdoutPipe", slog.Any("错误", err))
+		return
 	}
 	if err = cmd.Start(); err != nil {
-		log.Warn.Panicf("cmd.Run产生的错误:%v", err)
+		slog.Warn("cmd.Run", slog.Any("错误", err))
+		return
 	}
 	for {
 		tmp := make([]byte, 1024)
@@ -59,18 +106,19 @@ func rotate(in GetFileInfo.Info, direction, threads string) {
 		//写成输出日志
 		t := string(tmp)
 		t = replace.Replace(t)
-		log.TTY.Println(t)
+		fmt.Println(t)
 		if err != nil {
 			break
 		}
 	}
 	if err = cmd.Wait(); err != nil {
-		log.Warn.Panicf("命令执行中有错误产生:%v", err)
+		slog.Warn("cmd.Wait", slog.Any("错误", err))
+		return
 	}
 	err = os.RemoveAll(in.FullPath)
 	if err != nil {
-		log.Warn.Panicf("删除文件%v出现错误%v\n", in.FullPath, err)
+		slog.Warn("删除失败", slog.Any("源文件", in.FullPath), slog.Any("错误", err))
 	} else {
-		log.Debug.Printf("完成当前文件的处理:%s\n", dst)
+		slog.Info("删除成功", slog.Any("源文件", in.FullPath))
 	}
 }
